0135bb487115c4cb958785e47e2b75862f9df3d2
gfianu <delyouandme@gmail.com>
2023-07-15 18:06:11 -0400

    Update sdt_app.py

--- sdt_app.py
+++ sdt_app.py
@@ -12,6 +12,14 @@ import plotly as pl
 import plotly.express as px
 import streamlit as st
 
+# Streamlit introduction page
+st.title('Welcome to The Car Dealership!!')
+
+st.markdown("""
+This app performs simple analysis on the vehicle inventory of this dealership!
+""")
+# Side bar
+st.sidebar.header('User Input Features')
 
 # Retrieving data
 vehicles = pd.read_csv('vehicles_us.csv')
@@ -67,15 +75,6 @@ vehicle_types = vehicles['type'].sort_values().unique()
 # Vehicle condition
 vehicle_condition = vehicles['condition'].sort_values().unique()
 
-# Streamlit introduction page
-st.title('Welcome to The Car Dealership!!')
-
-st.markdown("""
-This app performs simple analysis on the vehicle inventory!
-""")
-# Side bar
-st.sidebar.header('User Input Features')
-
 # Side bar vehicle type selection
 selected_type = st.sidebar.multiselect('Vehicle types',vehicle_types, vehicle_types)
 
@@ -85,15 +84,13 @@ selected_condition = st.sidebar.multiselect('Vehicle condition',vehicle_conditio
 # Filtering data
 types_conditions = vehicles[(vehicles.type.isin(vehicle_types)) & (vehicles.condition.isin(vehicle_condition))]
 
-st.header('Display Vehicles by types and conditions')
+st.header('Display vehicles by types and conditions')
 st.write('Data Dimension: ' + str(types_conditions.shape[0]) + ' rows and ' + str(types_conditions.shape[1]) + ' columns.')
 st.dataframe(types_conditions)
-
 
 # Streamlit data exploration using plotly_express
 st.header('Analysis of vehicle types based on price (in USD) and condition')
 st.write(px.strip(vehicles, x='type', y = 'price', color = 'condition', hover_name = 'model'))
-
 
 # Streamlit data exploration using plotly_express
 st.header('Analysis on how vehicle condition and fuel affects price (in USD)')
@@ -112,18 +109,15 @@ st.header('A sunburst chart of vehicle inventory by fuel, type, and condition')
 st.write(px.sunburst(vehicles, color ='model', values = 'price', path= ['fuel','type', 'condition'] , 
             hover_name = 'model_year', height= 700))
 
-
 # Streamlit data exploration using plotly_express
 st.header('A treemap of vehicle inventory by fuel, type, model, condition, and price')
 st.write(px.treemap(vehicles, color ='transmission', values = 'odometer', path= ['fuel','type', 'model', 'condition', 'price'] , 
             hover_name = 'model_year', height= 700))
 
-
 # Streamlit data exploration using plotly_express
 st.header('Analysis of vehicle price by type, model, model year, and condition')
 st.write(px.scatter(vehicles, y ='model_year', x = 'type', hover_name = 'model', size = 'price', 
            color = 'condition', size_max = 80, height= 700))
-
 
 st.header('Price analysis')
 st.write("""
